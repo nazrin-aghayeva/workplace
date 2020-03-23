62ac8591af67fdaf9a42f9eba90848f6287c6dfb
nazrin <nazrinaghayeva@gmail.com>
2020-03-23 10:58:14 +0400

    --Recursion

---
+++ src/main/java/warmup20/SumArray.java
@@ -0,0 +1,18 @@
+package warmup20;
+
+import java.util.Random;
+
+public class SumArray {
+    static int fibo (int data) {
+        if (data<=1){
+            return data;
+        }
+      return fibo(data-1)+fibo(data-2);
+    }
+
+
+    public static void main(String[] args) {
+        int fibonacci = fibo(5);
+        System.out.println(fibonacci);
+    }
+}
