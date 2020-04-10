a98b3fc228edd599f36ea5a7212d8570fc10ad01
nazrin <nazrinaghayeva@gmail.com>
2020-04-10 11:15:22 +0400

    -- Java WEB lesson2

--- src/main/java/webLesson2/Calculator.java
+++ src/main/java/webLesson2/Calculator.java
@@ -4,24 +4,23 @@ import com.sun.codemodel.internal.JCase;
 
 public class Calculator {
   public String doOperation(String x, String y, String op) {
-    if (op == "multiply") {
+    if (op.equals("multiply")) {
       int result = Integer.parseInt(x) * Integer.parseInt(y);
       return String.valueOf(result);
-
     }
-    else if(op=="divide") {
+    else if(op.equals("divide")) {
       int result1 = Integer.parseInt(x) / Integer.parseInt(y);
       return String.valueOf(result1);
     }
-    else if(op=="subtract") {
+    else if(op.equals("subtract")) {
       int result2 = Integer.parseInt(x) - Integer.parseInt(y);
       return String.valueOf(result2);
     }
-    else if(op=="add") {
+    else if(op.equals("add")) {
       int result3 = Integer.parseInt(x) + Integer.parseInt(y);
       return String.valueOf(result3);
     }
-    else if(y=="0") {
+    else if(y.equals("0")) {
       return "division by zero";
     }
     else{
