67af874c3056cea33a5c5e8e94a97460712bf54b
nazrin <nazrinaghayeva@gmail.com>
2020-04-13 09:38:23 +0400

    -- Java WEB lesson3

---
+++ src/main/java/webLess2v2/CalcServlet.java
@@ -0,0 +1,32 @@
+package webLess2v2;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class CalcServlet extends HttpServlet {
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    // THE ONLY TASK IS TO EXTRACT THE PARAMS
+    String x = req.getParameter("x");
+    String y = req.getParameter("y");
+    String op = req.getParameter("op");
+
+    // THE ONLY TASK IS TO CALL THE BUSINESS LOGIC
+    Calculator c = new Calculator();
+    String res = c.doOperation(x, y, op);
+
+    // THE ONLY TASK IS TO PRINT THE RESULT
+    try (PrintWriter writer = resp.getWriter()) {
+      writer.write(res);
+    }
+//    try (OutputStream os = resp.getOutputStream()) {
+//      os.write();
+//    }
+  }
+}
---
+++ src/main/java/webLess2v2/ServerApp.java
@@ -0,0 +1,19 @@
+package webLess2v2;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class ServerApp {
+  public static void main(String[] args) throws Exception {
+    Server server = new Server(9000);
+    ServletContextHandler handler = new ServletContextHandler();
+
+    // the only task is to MAP the SERVLET to the PATH
+    handler.addServlet(new ServletHolder(new CalcServlet()), "/calc/*");
+
+    server.setHandler(handler);
+    server.start();
+    server.join();
+  }
+}
